5a9d44d47da28ab02588075745060f97e93c8220
Nathan Snyder <snyderxc@gmail.com>
2012-06-12 00:24:06 +0000

    IE Transition

---
+++ IEOnly.js
@@ -0,0 +1,13 @@
+    $(".expand").toggle(function (){
+        $(".content").animate({"margin-left": "60%"}, "slow");
+	}, function(){
+        $(".content").animate({"margin-left": "19.5%"}, "slow");
+	});
+    $(".expand").click(function () {
+    
+       //$('.content').toggleClass('open closed');
+       //$('.sidebar').toggleClass('open closed');
+       //$('.column').toggleClass('hidden shown');
+	   
+    });
+
