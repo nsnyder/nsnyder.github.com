aad946e682a7b3be1962e76b3f02b9bc5ebff841
Nathan Snyder <snyderxc@gmail.com>
2012-06-12 01:15:59 +0000

    IE7 Compatibility

--- IEOnly.js
+++ IEOnly.js
@@ -1,13 +1,10 @@
     $(".expand").toggle(function (){
         $(".content").animate({"margin-left": "60%"}, "slow");
+        $(this).text("Less")
+    	.stop();
 	}, function(){
         $(".content").animate({"margin-left": "19.5%"}, "slow");
+		$(this).text("More")
+		.stop();
 	});
-    $(".expand").click(function () {
-    
-       //$('.content').toggleClass('open closed');
-       //$('.sidebar').toggleClass('open closed');
-       //$('.column').toggleClass('hidden shown');
-	   
-    });
 
