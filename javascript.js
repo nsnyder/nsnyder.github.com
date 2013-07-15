7f9914c304456df81bdcfe7f150a7f787e5511ae
Nathan Snyder <snyderxc@gmail.com>
2013-07-14 23:49:46 -0400

    Finalized, with mobile, possible image updates on the way

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 $(window).load(function() {
     var cw = $('.extLink').css('height');
     $('.LeadLetter').css({'width':cw});
+    if($(window).width() > 600) {
+        var container = document.querySelector('.MasonryIfAvail');
+        var msnry = new Masonry( container, {
+            itemSelector: '.MasonryElem'
+        });
+    }
 });
 $(window).resize(function () {
     var cw = $('.extLink').css('height');
