af4c7d09bc43c2a25d6b366dbb17ff004840e360
Nathan Snyder <snyderxc@gmail.com>
2013-08-02 00:16:17 -0400

    Layout

--- javascript.js
+++ javascript.js
@@ -49,6 +49,16 @@ h[a]+'" class="twitter_fav_icon">Favorite</a></p>');c.push(b);a++}if(null==w){e=
 $(window).resize(function () {
     var cw = $('.extLink').css('height');
     $('.LeadLetter').css({'width':cw});
+    if($(window).width() > 600) {
+        var container = document.querySelector('.MasonryIfAvail');
+        var msnry = new Masonry( container, {
+            itemSelector: '.MasonryElem'
+        });
+        container = document.querySelector('#LinkContainer .MasonryIfAvail');
+        var msnry2 = new Masonry( container, {
+            itemSelector: '.MasonryElem'
+        });
+    }
 });
 function processPosts(posts) {
     $('#latestPostTitle').html('<a href="'+ posts.feed.entry[0].link[4].href + '">' + posts.feed.entry[0].title.$t + '</a>');
