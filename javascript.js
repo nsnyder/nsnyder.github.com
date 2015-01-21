ceadcc843aba9a551c4a2366adc4a743c817e46b
Nathan Snyder <nathan@snydern.com>
2015-01-21 12:53:58 -0500

    tweak

--- javascript.js
+++ javascript.js
@@ -90,6 +90,7 @@ $(window).resize(function () {
     var cw = $('.extLink').css('height');
     $('.LeadLetter').css({'width':cw});
     if($(window).width() > 600) {
+		$('#pageNav').css('top', 'auto').css('left', 'auto');
         var container = document.querySelector('.MasonryIfAvail');
         var msnry = new Masonry( container, {
             itemSelector: '.MasonryElem'
