3ac936ada581f244149a3ff777958f2473d6d4bf
Nathan Snyder <nathan@snydern.com>
2020-03-28 17:17:37 -0400

    Gut everything. We're building this from the ground up.

--- IE7.js
+++
@@ -1,9 +0,0 @@
-    $(".expand").toggle(function (){
-        $(".content").animate({"margin-left": "0%"}, "slow");
-        $(this).text("Less")
-    	.stop();
-	}, function(){
-        $(".content").animate({"margin-left": "-40%"}, "slow");
-		$(this).text("More")
-		.stop();
-	});
