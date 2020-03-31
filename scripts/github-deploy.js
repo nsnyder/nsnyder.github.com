a9ad40fff9e30099f5bf385c33497214e4059baa
Nathan Snyder <nathan@snydern.com>
2020-03-31 19:30:30 -0400

    Prepare automatic deploy

---
+++ scripts/github-deploy.js
@@ -0,0 +1,28 @@
+// Thanks to: https://dev.to/rolanddoda/deploy-to-github-pages-like-a-pro-with-github-actions-4hdg
+
+const execa = require("execa");
+const fs = require("fs");
+
+const sourceBranch = 'staging';
+const targetBranch = 'master';
+
+(async () => {
+  try {
+    await execa("git", ["checkout", "--orphan", targetBranch]);
+    console.log("Building...");
+    await execa("npm", ["run", "build"]);
+    // Understand if it's dist or build folder
+    const folderName = fs.existsSync("dist") ? "dist" : "build";
+    await execa("git", ["--work-tree", folderName, "add", "--all"]);
+    await execa("git", ["--work-tree", folderName, "commit", "-m", targetBranch]);
+    console.log("Pushing to gh-pages...");
+    await execa("git", ["push", "origin", `HEAD:${ targetBranch }`, "--force"]);
+    await execa("rm", ["-r", folderName]);
+    await execa("git", ["checkout", "-f", sourceBranch]);
+    await execa("git", ["branch", "-D", targetBranch]);
+    console.log("Successfully deployed!");
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+})();
