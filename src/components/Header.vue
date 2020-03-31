e45542bce77e210c20d2257fdaad8c4508165ae5
Nathan Snyder <nathan@snydern.com>
2020-03-30 21:00:53 -0400

    Add rudimentary gradient plugin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="bg-red-400 min-h-screen flex items-center align-middle justify-center">
-    <div class="font-display text-5xl md:text-6xl">
+  <div class="bg-gradient-purple-blue min-h-screen flex flex-col p-5 items-center align-middle justify-center">
+    <div class="font-display text-5xl md:text-6xl my-5">
       Nathan Snyder
     </div>
+    <div class="text-center my-5 max-w-xl">
+      Hi! I'm a developer at The Restaurant Store. I love tackling complex projects,
+      learning new things, and building tools that help people get things done
+      quickly and effectively.
+    </div>
+    <a href="mailto:nathan@snydern.com" class="my-5 border-2 px-4 py-2 text-white font-bold border-white rounded-full">
+      Contact Me
+    </a>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
   theme: {
     extend: {},
+    gradients: theme => ({
+      'blue-green': ['-45deg', theme('colors.blue.500'), theme('colors.green.500')],
+      'purple-blue': ['-45deg', theme('colors.purple.500'), theme('colors.blue.500')]
+    }),
     fontFamily: {
       display: ['Satisfy', 'cursive']
     }
   },
-  variants: {}
+  variants: {},
+  plugins: [
+    require('./plugins/gradient')
+  ]
 };
