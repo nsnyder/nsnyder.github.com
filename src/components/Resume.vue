668ac0e3ac724cbb17ca757bcb9435f4c7795446
Nathan Snyder <nathan@snydern.com>
2020-04-11 14:31:37 -0400

    Remove old resources and add logos
    Added logos for:
    The Restaurant Store
    Solo Labs
    Grove City College
    
    Images do not reflect an endorsement by their respective organizations

--- src/components/Resume.vue
+++ src/components/Resume.vue
@@ -3,9 +3,9 @@
     <div class="px-2 mx-auto my-12 lg:max-w-6xl">
       <!-- TODO: Pick a great font for headings. -->
       <h2 class="py-2 text-xl font-bold">Experience</h2>
-      <div class="grid grid-cols-1 gap-2 md:grid-cols-3" ref="experience-grid">
+      <div class="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3" ref="experience-grid">
         <div v-for="(entry, index) in experienceEntries"
-          class="h-32 p-2 duration-500 transform border-l-8 rounded bg-gradient-shades-of-gray"
+          class="h-32 p-2 duration-500 transform rounded bg-gradient-shades-of-gray"
           :key="index"
           :class="Object.assign({
             'opacity-0 -translate-x-4': (index > visibleElementsCount - 1)
@@ -23,6 +23,7 @@
     name: 'Resume',
 
     data() {
+      // eslint-disable-next-line no-unused-vars
       const createClassObjectFromArray = (array) => array.reduce((previous, entry) => 
         Object.assign(previous, { [entry]: true  }),
         {}
@@ -31,15 +32,15 @@
       return {
         experienceEntries: [
           {
-            // classes: createClassObjectFromArray(['border-green-700']),
+            classes: createClassObjectFromArray(['logo-mark trs-background']),
             title: 'The Restaurant Store',
           },
           {
-            // classes: createClassObjectFromArray(['border-red-700']),
+            classes: createClassObjectFromArray(['logo-mark solo-background']),
             title: 'Solo Labs',
           },
           {
-            // classes: createClassObjectFromArray(['border-red-700']),
+            classes: createClassObjectFromArray(['logo-mark gcc-background']),
             title: 'Grove City College',
           }
         ],
@@ -72,3 +73,29 @@
     }
   }
 </script>
+
+<style scoped>
+  .logo-mark::after {
+    content: "";
+    background-size: auto 5em;
+    opacity: 0.1;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    z-index: -1;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-image: var(--background-image);
+  }
+  .gcc-background::after {
+    --background-image: url('/img/gcc-logo.png');
+  }
+  .solo-background::after {
+    --background-image: url('/img/solo-logo.svg');
+  }
+  .trs-background::after {
+    --background-image: url('/img/trs-logo-large-cropped.png');
+  }
+</style>
