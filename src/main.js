cd6be08a27f6f473e219aaabe2d41d18182aad82
Nathan Snyder <nathan@snydern.com>
2020-04-16 17:45:00 -0400

    Install CompositionAPI

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue';
+import VueCompositionApi from '@vue/composition-api';
 import App from './App.vue';
 import constants from '~/constants';
 
-Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
 
 new Vue({
   data: {
